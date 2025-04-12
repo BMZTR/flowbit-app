abf3c7ec29369afa14cd10428edec1a374079d8f
BMZTR <yass7770@gmail.com>
2025-04-12 11:37:53 +0000

    Tester index.js minimal et confirmer vercel.json pour routage SPA

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-function Login() {
-  return <div><h1>Page de Connexion</h1><p>Veuillez vous connecter.</p></div>;
-}
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </BrowserRouter>
 );
