1569803458ca011346745252c720fda51d27cd5f
BMZTR <yass7770@gmail.com>
2025-04-12 11:24:14 +0000

    Tester index.js minimal pour confirmer la redirection

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import Register from './Register';
-import ForgotPassword from './ForgotPassword';
-import MainApp from './App';
+
+function Login() {
+  return <div><h1>Page de Connexion</h1><p>Veuillez vous connecter.</p></div>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/app" element={<MainApp />} />
       <Route path="/" element={<Login />} />
     </Routes>
   </BrowserRouter>
